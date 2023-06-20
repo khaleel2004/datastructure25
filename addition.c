d3e704f732ed424374ca8fa9fc53aae27f3cfb19
khaleel2004 <137160654+khaleel2004@users.noreply.github.com>
2023-06-20 14:30:44 +0530

    addition.c

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value ;");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("%d\n",c);
+c=a-b;
+printf("%d\n",c);
+c=a*b;
+printf("%d\n",c);
+c=a/b;
+printf("%d\n",c);
+getch();
+}
